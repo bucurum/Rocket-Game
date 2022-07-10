e6acd0a6522a93c94f8acf0037682d3594ad126b
bucurum <bucurum18@posta.pau.edu.tr>
2022-07-10 19:11:33 +0300

    added moveable obstacle and 4 level desinged

---
+++ Assets/Scripts/Oscillator.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Oscillator : MonoBehaviour
+{
+    Vector3 startingPosition;
+    [SerializeField] Vector3 movementVector;
+    [SerializeField] float period = 2f;
+
+    float movementFactor;
+
+    
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startingPosition = transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        NewMethod();
+    }
+
+    private void NewMethod()
+    {
+        if(period != 0)
+        {
+            float cycles = Time.time / period; //Continually growing over time
+
+            const float tau = Mathf.PI * 2; //constant value for 6.283
+            float rawSinWave = Mathf.Sin(cycles * tau); //going -1 to 1
+
+            movementFactor = (rawSinWave + 1f) / 2f; // recalculated to go from 0 to 1
+
+            Vector3 offset = movementFactor * movementVector;
+            transform.position = startingPosition + offset;
+        }
+        else
+        {
+            return;
+        }
+       
+
+    }
+}
